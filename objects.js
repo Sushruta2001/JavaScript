0ddfcc35def609a38e1a28b69cef9f3d467b667b
Sushruta Kayal <sushrutakayal6@gmail.com>
2025-11-26 20:42:37 +0530

    obj

---
+++ objects.js
@@ -0,0 +1,36 @@
+// let a = new Object()
+
+// console.log(a);
+
+// a.name = 'sushruta'
+// a.id = 123
+// a.age = 24
+
+// console.log(a);
+
+let sym = Symbol('hello')
+
+
+let a = {
+    id : 123,
+    name : 'Sushruta',
+    age : 24,
+    [sym] : 'mysym'
+}
+console.log(a);
+
+console.log(Object.keys(a))   //returns the keys of the object
+console.log(Object.values(a)) //returns the values of the object
+console.log(Object.entries(a))  ////returns the keys and value pairs of the object
+console.log(Object.getOwnPropertySymbols(a))  //returns anysymbol present in the object
+
+let b = {
+    id2 : 234,
+    name2 : 'abc',
+    age2 : 30
+}
+
+console.log(Object.assign({},a,b));   //joins the  2 objects 
+let c = {...a,...b}                     //using spread operator we can also do the same
+console.log(c);        
+
